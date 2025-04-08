7b94977b9508d0cde8ae9192a39c475cf8d95199
Derrick Kearney <d_kearney@bellaliant.net>
2025-04-08 19:35:36 -0300

    Add code for 2 models tasks and contacts admin file

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task, Contacts
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'task',
+        'annual_task',
+        'assigned_by',
+        'task_details',
+        'date_assigned',
+        'date_started',
+        'date_completed',
+    )
+    list_filter = (
+        'annual_task',
+        'assigned_by',
+        'date_assigned',
+        'date_started',
+        'date_completed',
+    )
+
+
+@admin.register(Contacts)
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'contact_name',
+        'specialty',
+        'primary_phone',
+        'secondary_phone',
+        'contact_email',
+        'notes',
+    )
