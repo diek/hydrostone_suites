1f748bdf40c412c781a5c80a07d660bceb9f3b94
Derrick Kearney <d_kearney@bellaliant.net>
2025-04-08 18:38:58 -0300

    Add default for is_resident, and allow no phone numbers

--- users/models.py
+++ users/models.py
@@ -8,9 +8,9 @@ from .managers import CustomUserManager
 class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(_("email address"), unique=True)
-    is_resident = models.BooleanField()
-    primary_phone = PhoneNumberField()
-    secondary_phone = PhoneNumberField(blank=True)
+    is_resident = models.BooleanField(default=True)
+    primary_phone = PhoneNumberField(blank=True, null=True )
+    secondary_phone = PhoneNumberField(blank=True, null=True)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
