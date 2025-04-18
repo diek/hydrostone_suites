1f68505e9f366d87765be6cc2219737f85f10fb3
Derrick Kearney <d_kearney@bellaliant.net>
2025-04-18 16:43:30 -0300

    Change debug to False

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ["hydrostonesuites.com", "170.64.142.53"]
 ALLOWED_HOSTS = ['*']
